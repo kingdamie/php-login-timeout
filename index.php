e701efc178c49224dfccb1f940ab379c9cfd07f1
king damie <110835422+kingdamie@users.noreply.github.com>
2023-09-22 23:17:09 +0100

    index.php

--- index.php
+++ index.php
@@ -5,34 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>king's motor</title>
     <link rel="stylesheet" href="index.css" />
-    <style>
-      header {
-        display: grid;
-        grid-template-columns: 2fr 3fr 1fr;
-        align-items: center;
-      }
-      nav ul {
-        list-style: none;
-        display: grid;
-        grid-template-columns: 1fr 1fr;
-        justify-content: center;
-      }
-      nav ul li a {
-        text-decoration: none;
-        color: brown;
-      }
-      button {
-        border: none;
-        background-color: brown;
-        cursor: pointer;
-        padding: 1rem;
-      }
-      button a {
-        text-decoration: none;
-        color: aliceblue;
-        
-      }
-    </style>
   </head>
   <body>
     <header>
